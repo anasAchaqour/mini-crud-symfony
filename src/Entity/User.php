5e1a6d6b2c1f27f4b962ec639299f42c7fc0187e
anasAchaqour <98333509+anasAchaqour@users.noreply.github.com>
2024-09-19 16:08:36 +0100

    .

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -148,4 +148,8 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
 
         return $this;
     }
+
+    public function __toString(): string{
+        return $this->email;
+    }
 }
